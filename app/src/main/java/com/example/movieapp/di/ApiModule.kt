fbc229db4c33c00632dea5e6e7f87d797681b853
Kyryll <kyryllrusalin@gmail.com>
2023-02-06 19:33:44 +0200

    Implemented dependencies injections

---
+++ app/src/main/java/com/example/movieapp/di/ApiModule.kt
@@ -0,0 +1,76 @@
+package com.example.movieapp.di
+
+import com.example.movieapp.BuildConfig
+import com.example.movieapp.api.ApiServices
+import com.example.movieapp.utils.Constants.API_KEY
+import com.example.movieapp.utils.Constants.BASE_URL
+import com.example.movieapp.utils.Constants.NETWORK_TIMEOUT
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    @Provides
+    @Singleton
+    fun ProvideBaseUrl() = BASE_URL
+
+    @Provides
+    @Singleton
+    fun ConnectionTimeOut() = NETWORK_TIMEOUT
+
+    @Provides
+    @Singleton
+    fun ProvideGson(): Gson = GsonBuilder().setLenient().create()
+
+    @Provides
+    @Singleton
+    fun ProvideOKHttpClient() = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val requestInterceptor = Interceptor { chain ->
+            val url = chain.request()
+                .url
+                .newBuilder()
+                .addQueryParameter("api_key", API_KEY)
+                .build()
+
+            val request = chain.request()
+                .newBuilder()
+                .url(url)
+                .build()
+
+            return@Interceptor chain.proceed(request)
+        }
+
+        OkHttpClient
+            .Builder()
+            .addInterceptor(requestInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    } else {
+        OkHttpClient
+            .Builder()
+            .build()
+    }
+    
+    fun provideRetrofit(baseUrl: String, gson: Gson, client: OkHttpClient): ApiServices =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(ApiServices::class.java)
+}
--- app/src/main/java/com/example/movieapp/utils/Constants.kt
+++ app/src/main/java/com/example/movieapp/utils/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val API_KEY = "4c2c16d168926a235bfe928f070726c3"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
+
+    const val NETWORK_TIMEOUT = 60L
 }
